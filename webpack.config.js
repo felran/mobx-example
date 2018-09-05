0f27a448371744e9918ba44899c1cd62cb013db3
罗然 <hzluoran@corp.netease.com>
2018-09-05 14:26:58 +0800

    add hmr

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,18 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
-const env = process.env.NODE_ENV;
 module.exports = {
-    mode:env,
+    mode:'development',
     entry:[
         'webpack-hot-middleware/client?reload=true',
         path.resolve(__dirname,'./app/index.js')
     ],
     output:{
-        filename:env==='development'?'[name].js':'[name].[hash].js',
+        filename:'[name].js',
         path:path.resolve(__dirname,'dist'),
         publicPath:'/'
     },
-    devtool:env==='development'?'inline-source-map':'',
+    devtool:'inline-source-map',
     module:{
         rules:[{
             test:/\.js$/,
@@ -35,5 +34,5 @@ module.exports = {
             template: path.resolve(__dirname, './public/index.html'),
         }),
         new webpack.HotModuleReplacementPlugin(),
-    ],
+    ]
 }
